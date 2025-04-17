bdb717e36121a6b0c78debcb4682fc14dbd80843
Aliriza Tasdemir <85988176+artrza@users.noreply.github.com>
2025-04-17 11:08:28 +0200

    Delete entrypoint.sh

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# entrypoint.sh
-
-echo "Welcome $APP_WELCOME_MESSAGE!"
-echo "I'm running on node $MY_NODE_NAME with the pod name $MY_POD_NAME."
-echo "Now sleeping..."
-sleep 3600
-
